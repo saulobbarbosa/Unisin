1eb47e65e647c5d17178c284b88474b043878d19
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2025-09-28 20:35:15 -0300

    Header Pronto + Modal

--- frontend/src/components/layout/headers/HeaderAluno.jsx
+++ frontend/src/components/layout/headers/HeaderAluno.jsx
@@ -4,6 +4,7 @@ import HeaderStyle from "./header.module.css";
 // Import Componentes
 
 export default function CompHeaderAluno(){
+    const [mostrar, setMostrar] = useState(false);
     const [nivel, setNivel] = useState(20);
     const [moeda, setMoeda] = useState(2500);
 
@@ -22,12 +23,56 @@ export default function CompHeaderAluno(){
                     className={HeaderStyle.imgMoeda} draggable="false" />
                     <p>{moeda}</p>
                 </div>
-                <div className={HeaderStyle.divPerfil + " " + HeaderStyle.divNMP}>
+                <div className={HeaderStyle.divPerfil + " " + HeaderStyle.divNMP}
+                    onClick={() => { setMostrar(!mostrar) }}
+                >
                     <i className="fa-solid fa-user" style={{ fontSize: "2.5rem", color: "#000" }}></i>
                 </div>
             </div>
             {/* Parte do Modal */}
-            
+            <div className={`${HeaderStyle.divModalNavegacao} ${mostrar ? HeaderStyle.show : HeaderStyle.hide}`}>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-house" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Home</h1>
+                </div>
+                <hr className={HeaderStyle.linhaModal}/>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-people-group" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Amigos</h1>
+                </div>
+                <hr className={HeaderStyle.linhaModal}/>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-store" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Loja</h1>
+                </div>
+                <hr className={HeaderStyle.linhaModal}/>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-medal" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Conquistas</h1>
+                </div>
+                <hr className={HeaderStyle.linhaModal}/>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-ranking-star" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Ranking</h1>
+                </div>
+                <hr className={HeaderStyle.linhaModal}/>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-user" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Perfil</h1>
+                </div>
+                <hr className={HeaderStyle.linhaModal}/>
+                <div className={HeaderStyle.divEscolhas}>
+                    <i className="fa-solid fa-right-from-bracket" style={{ fontSize: "2.5rem", color: "#000", 
+                    marginLeft: "2rem" }}></i>
+                    <h1 className={HeaderStyle.textEscolhas}>Sair</h1>
+                </div>
+            </div>
         </div>
     );
 }
