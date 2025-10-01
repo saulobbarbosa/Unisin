abf60d38c06d957f11c39044aa8ea1cc3c5a5731
GFreitas-033 <guilhermelfreitas190307@gmail.com>
2025-10-01 00:11:13 -0300

    Tela de Trilha de Atividade Pronta

--- frontend/src/components/aluno/trilha/Trilha.jsx
+++ frontend/src/components/aluno/trilha/Trilha.jsx
@@ -9,6 +9,11 @@ import Barra from "../barra-top/BarraTop";
 
 const etapas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+const classes = [
+    "primeiro", "segundo", "terceiro",
+    "quarto", "quinto", "sexto",
+]
+
 export default function TelaAlunoTrilha({ nomeMateria }){
     const [level, setLevel] = useState("4");
     
@@ -18,6 +23,25 @@ export default function TelaAlunoTrilha({ nomeMateria }){
             <main className={Ajuste.container}>
                 <Barra nomeMateria={nomeMateria} level={level}/>
                 
+                <div className={Style.divConquista}>
+                    <h2 className={Style.tituloConquista}>Conquistas</h2>
+                </div>
+
+                <div className={Style.trilha}>
+                    {etapas.map((etapa, index) => {
+                        const classeExtra = classes[index % classes.length];
+                        const deslocamento = Math.sin(index * 0.7) * 6; // amplitude horizontal
+                        return (
+                        <div
+                            key={etapa}
+                            className={`${Style.etapa} ${Style[classeExtra]}`}
+                            style={{ marginLeft: `${deslocamento}rem`, marginTop: "1rem" }}
+                        >
+                            {etapa}
+                        </div>
+                        );
+                    })}
+                </div>
             </main>
         </div>
     )
